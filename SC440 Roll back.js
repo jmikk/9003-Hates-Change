5b329b3a1befdbcd8dde0d521b4cc53abf746db4
jmikk <buldog.buldog26@gmail.com>
2023-04-28 08:43:11 -0500

    Update SC440 Roll back.js

--- SC440 Roll back.js
+++ SC440 Roll back.js
@@ -6,6 +6,7 @@
 // @match        https://www.nationstates.net/nation=*
 // @match        https://www.nationstates.net/page=*/card=*
 // @match        https://www.nationstates.net/page=deck/nation=*
+// @match        https://www.nationstates.net/region=*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
