ec9062099c83394ce05ef48e5f2afe75f03fbe36
jmikk <buldog.buldog26@gmail.com>
2023-04-28 08:37:36 -0500

    Create SC440 Roll back.js

---
+++ SC440 Roll back.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Change Image Based on Security Council Resolution Number
+// @namespace    your-namespace-here
+// @version      1
+// @description  Changes the image if Security Council Resolution number is less than or equal to 440
+// @match        https://www.nationstates.net/nation=*
+// @match        https://www.nationstates.net/page=*/card=*
+// @match        https://www.nationstates.net/page=deck/nation=*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const MAX_RESOLUTION_NUMBER = 440; // Change this to the maximum Security Council Resolution number for which the image should be changed
+
+    const badgeElements = document.querySelectorAll('.badge[title*="Security Council Resolution #"]');
+
+    badgeElements.forEach((imageElement) => {
+        const title = imageElement.getAttribute('title');
+        const resolutionNumber = parseInt(title.match(/Security Council Resolution # (\d+)/)[1]);
+
+        if (resolutionNumber <= MAX_RESOLUTION_NUMBER) {
+            if (title.includes('Condemned')) {
+                imageElement.setAttribute('src', 'https://i.imgur.com/rCdUOAi.png');
+            } else if (title.includes('Commended')) {
+                imageElement.setAttribute('src', 'https://i.imgur.com/yPHKM5g.png');
+            }
+        }
+    });
+})();
